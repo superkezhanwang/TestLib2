41ea385a1295a32ca32698024e1cef33bbbd6ab5
superkezhanwang <1143249327@qq.com>
2016-09-21 16:43:40 +0800

    testlib2
    testlib2

---
+++ src/main/java/com/dnd/testlib2/HelloClass.java
@@ -0,0 +1,10 @@
+package com.dnd.testlib2;
+
+/**
+ * Created by ningdan on 2016/9/21.
+ */
+public class HelloClass {
+    public void sayHello(){
+        System.out.println("Hello world");
+    }
+}
